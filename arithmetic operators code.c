9660da7c001068eb0274391dd70c3d2ace42f4b8
Aditya Bawnoo <2020a1r154@mietjammu.in>
2021-03-19 10:47:12 +0530

    Files added

---
+++ arithmetic operators code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,add,subt,mult;
+	float divide;
+	printf("Enter any two numbers");
+	scanf("%d%d",&a,&b);
+	add=a+b;
+	subt=a-b;
+	mult=a*b;
+	divide=a/b;
+	printf("\nAddition of the numbers =%d\n",add);
+	printf("Subtraction of two numbers=%d\n",subt);
+	printf("multiplication of the two numbers =%d\n",mult);
+	printf("Divison of the two numbers =%f\n",divide);
+	return 0;
+}
+	
