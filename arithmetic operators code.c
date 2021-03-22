978e511a5711d529da206e79b7c86d4f311f22a8
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-03-22 11:00:10 +0530

    Update arithmetic operators code.c

--- arithmetic operators code.c
+++ arithmetic operators code.c
@@ -7,11 +7,11 @@ int main()
 	int a,b,add,subt,mult;   // Variable declaration of int data type
 	float divide;          // Variable declaration of float data type
 	printf("Enter any two numbers"); // Printf function calling for Two numbers 
-	scanf("%d%d",&a,&b);
-	add=a+b;
-	subt=a-b;
-	mult=a*b;
-	divide=a/b;
+	scanf("%d%d",&a,&b);  // Scanf function calling for taking Input of two numbers.
+	add=a+b;           // Addition of two numbers
+	subt=a-b;          // Subtraction of two numbers
+	mult=a*b;         // Multiplication of two numbers
+	divide=a/b;      //Divison of two numbers
 	printf("\nAddition of the numbers =%d\n",add);  // Printf function calling for Addition of two numbers
 	printf("Subtraction of two numbers=%d\n",subt);  // Printf function calling for Subtraction of two numbers
 	printf("multiplication of the two numbers =%d\n",mult); // Printf function calling for multiplication of two numbers
