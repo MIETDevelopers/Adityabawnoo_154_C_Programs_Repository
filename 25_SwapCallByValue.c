664c4b293a2b6145bbea4ade4bb7d7ce5508211c
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 18:46:12 +0530

    Update 25_SwapCallByValue.c

--- 25_SwapCallByValue.c
+++ 25_SwapCallByValue.c
@@ -1,9 +1,11 @@
-//A C Program to swap two numbers using call by value.              31/March/2021
-#include<stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : A C Program to swap two numbers using call by value.              
+#include<stdio.h> // preprocessive directive to include standard input and output header file
 void swap(int, int);
 int main(){
     int a, b;
-    printf("Enter two values: ");
+    printf("Enter two values: "); // printf function calling for two values
     scanf("%d %d", &a, &b);//Taking input from user.
     swap(a,b);
 }
