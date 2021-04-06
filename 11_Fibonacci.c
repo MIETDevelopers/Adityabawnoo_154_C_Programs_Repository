d790a5c00d29cea40fc557755d7709530725aebd
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 17:42:24 +0530

    Update 11_Fibonacci.c

--- 11_Fibonacci.c
+++ 11_Fibonacci.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : Program for fibonacci series
+#include<stdio.h> // Preprocessive directive to include standard input and output header file.
  
-int main(){
+int main(){  // start of the main body
    int n, first = 0, second = 1, next, c; //For the reference to the formuls of the series.
  
-   printf("Enter the number of terms\n");
-   scanf("%d",&n); //This will scan (read) the number entered by user.
+   printf("Enter the number of terms\n");  // printf function calling for number of terms
+   scanf("%d",&n); // scanf function calling for the number of terms entered by user.
  
-   printf("First %d terms of Fibonacci series are :-\n",n);
+   printf("First %d terms of Fibonacci series are :-\n",n);  
  
    for ( c = 0 ; c < n ; c++ ) //ForLoop function for the reference to the formuls of the series.
    {
@@ -22,4 +25,4 @@ int main(){
       printf("%d\n",next); //This will give the outpu.
    }
    return 0;
-}
+} // end of the main body
