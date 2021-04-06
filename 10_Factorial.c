79f50028ea29be0c899ad02d8fead88888ff7ad6
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 17:37:35 +0530

    Update 10_Factorial.c

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -1,13 +1,16 @@
-//A C Program to calculate Factorial of a number.                18/March/2021
-#include<stdio.h>  
-int main()    
-{    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    //It will scan the number entered by user.
-    for(i=1;i<=number;i++){    
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : C Program to calculate Factorial of a number. 
+
+#include<stdio.h> // Preprocessive directory to include input and output header file  
+int main()       
+{    // start of the main body
+ int i,fact=1,number;    // variable declaration
+ printf("Enter a number: ");    // printf function calling for entering number
+  scanf("%d",&number);    // scanf function calling for taking input of  number entered by user.
+    for(i=1;i<=number;i++){    // loop
       fact=fact*i;   //Formula 
   }    
-  printf("Factorial of %d is: %d",number,fact);    //This will give us output.
+  printf("Factorial of %d is: %d",number,fact);    //printf function calling for output.
 return 0;  
-}
+} // end of the main body
