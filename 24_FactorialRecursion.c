90bd3281d905365a2db48725a50b32ae21f44341
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 18:40:32 +0530

    Update 24_FactorialRecursion.c

--- 24_FactorialRecursion.c
+++ 24_FactorialRecursion.c
@@ -1,10 +1,12 @@
-//A C Program to print factorial using recursion.	 			20/March/2021
-#include<stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : A C Program to print factorial using recursion.	 			20/March/2021
+#include<stdio.h> // preprocessive directive to include standard input and output header file
 	long int multiplyNumbers(int n);
-	int main() {
-    	int n;
-    	printf("Enter a positive integer: ");
-    	scanf("%d",&n);
+	int main() { // start of the main body
+    	int n; // variable declaration
+    	printf("Enter a positive integer: "); // printf function calling for positive integer
+    	scanf("%d",&n); // scanf function calling for talking input of the numbers entered by the user
     	printf("Factorial of %d = %ld", n, multiplyNumbers(n));
     	return 0;
 	}
