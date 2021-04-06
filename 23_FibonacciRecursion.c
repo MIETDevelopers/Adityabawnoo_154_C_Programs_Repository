85eaf7755ae5dd5ac4eec593aceffe30f7c1d773
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 18:37:00 +0530

    Update 23_FibonacciRecursion.c

--- 23_FibonacciRecursion.c
+++ 23_FibonacciRecursion.c
@@ -1,9 +1,11 @@
-//A C Program to print Fibonacci series using recursion.	 			20/March/2021
-#include<stdio.h>    
-int main(){    
-	int n1=0,n2=1,n3,i,number;    
-	printf("Enter the number of elements: ");    
-	scanf("%d",&number);    
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose :  C Program to print Fibonacci series using recursion
+#include<stdio.h>    // preprocessive directive to include standard input and output header file
+int main(){    // start of the main body
+	int n1=0,n2=1,n3,i,number;    // variable declaration
+	printf("Enter the number of elements: ");    // printf function calling for number of elements entered by the user
+	scanf("%d",&number);    // scanf function calling for taking input of the number of elements entered by the user.
 	printf(" %d %d",n1,n2);//Printing 0 and 1.   
 	for(i=2;i<number;++i)//Loop starts from 2 because 0 and 1 are already printed.   
 		{	//This will give output.
