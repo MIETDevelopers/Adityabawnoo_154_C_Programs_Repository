c37a982cdaa6e773d98264764b48c93c8e80eb52
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 18:44:02 +0530

    Update 25_SwapCallByReference.c

--- 25_SwapCallByReference.c
+++ 25_SwapCallByReference.c
@@ -1,14 +1,16 @@
-//A C Program to swap two numbers using call by reference.               20/March/2021
-#include <stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : A C Program to swap two numbers using call by reference.               
+#include <stdio.h> // preprocessive directive to include standard input and output header file
  
 void swap(int*, int*);
  
 int main()
-{
-   int x, y;
+{ // start of the main body
+   int x, y; // variable declaration
  
-   printf("Enter the value of x and y\n");
-   scanf("%d%d",&x,&y);
+   printf("Enter the value of x and y\n"); // printf function calling for two numbers ( x and y)
+   scanf("%d%d",&x,&y);  //  scanf function calling for taking input of two numbers (x and y) entered by the user
  
    printf("Before Swapping\nx = %d\ny = %d\n", x, y);
  
