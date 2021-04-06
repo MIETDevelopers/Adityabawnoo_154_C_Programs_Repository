6d620b0f89bf6297eee842c92be4b2c4b5f9ac68
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 17:55:03 +0530

    Update 14_SumOfDigits.c

--- 14_SumOfDigits.c
+++ 14_SumOfDigits.c
@@ -1,11 +1,13 @@
-//A C Program to Find Sum of Digits of a Number using While Loop.
-#include <stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : C Program to Find Sum of Digits of a Number using While Loop.
+#include <stdio.h> //  preprocessive directory to include standard input and output header file 
 int main()
-{
+{ // start of the main body
   int Number, Reminder, Sum=0;
 
-  printf("Please Enter any number\n");
-  scanf("%d", &Number);//This will scan (read) the number entered by user.
+  printf("Please Enter any number\n"); // printf function calling for number 
+  scanf("%d", &Number);// scanf function calling for taking input of the number entered by user.
 
   while(Number > 0)//Using while loop.
   {
@@ -15,7 +17,7 @@ int main()
      Number = Number / 10;
   }
 
-  printf("Sum of the digits of Given Number = %d\n", Sum);
+  printf("Sum of the digits of Given Number = %d\n", Sum); // printf function calling for output
 
   return 0;
-}
+} // end of the main body
