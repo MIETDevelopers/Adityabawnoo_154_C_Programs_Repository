16d790ec3b0da9af115c0aa771c5f379836ffb03
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 17:13:25 +0530

    Update 08_EuclideanDistance.c

--- 08_EuclideanDistance.c
+++ 08_EuclideanDistance.c
@@ -1,20 +1,34 @@
-//A C Program to calculate distance between two points.
-#include<stdio.h> //Including Standard input & output header file from library. 
+//Author name : Aditya Bawnoo
+creation date : 22 March 2021
+// Purpose : C Program to calculate distance between two points.
+#include<stdio.h> // preprocessive directory to include standard input and output. 
 #include<math.h>  //Including maths header file from library/
   
 int main()  
-{  
+{  // start of the main body
     float x1, y1, x2, y2, distance; //Declaring variables. 
   
-    printf("Enter point 1 (x1, y1)\n");  
-    scanf("%f %f", &x1, &y1);  //Scanning coordinates for first point.
+    printf("Enter x1"); // Printf function calling for x1
   
-    printf("Enter point 2 (x2, y2)\n");  
-    scanf("%f %f", &x2, &y2);  //Scanning coordinates for second point.
+    scanf("%f" &x1);  // scanf function calling for taking input of x1 by the user
+  
+    printf("Enter X2"); // Printf function calling for x2
+  
+    scanf("%f" &x2);  // scanf function calling for taking input of x2 by the user 
+  
+    printf("Enter y1"); // printf function calling for y1.
+  
+   scanf("%f",&y1);     // scanf function calling for taking input of y1 by the user
+  
+   printf("Enter y2");  // printf function calling for entering y2
+  
+   scanf("%f",&y2);   // scanf function calling for taking input of y2 by the user.
+  
+  
   
     distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) ); //Euclidean Distance's formula. 
   
     printf("Distance between (%0.2f, %0.2f) and (%0.2f, %0.2f) is %0.2f\n", x1, y1, x2, y2, distance); //Displaying calculated answer. 
   
     return 0;
-}
+} // end of the main body
