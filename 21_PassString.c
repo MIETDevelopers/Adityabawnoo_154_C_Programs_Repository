ab463c8d9fca0fce935f276097573b9f7e9a44e3
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 18:25:40 +0530

    Update 21_PassString.c

--- 21_PassString.c
+++ 21_PassString.c
@@ -1,11 +1,13 @@
-//A C Program for passing string to a Function. 			19/March/2021
-#include <stdio.h>
+//Author name : Aditya Bawnoo 
+//Creation date : 20 March 2021
+// Purpose : A C Program for passing string to a Function. 			
+#include <stdio.h> // preprocessive directory to include standard input and output header file
 void displayString(char str[]);
 
 int main()
-{
-    char str[50];
-    printf("Enter String: \n");
+{ // start of the main body
+    char str[50]; // Array declaration
+    printf("Enter String: \n"); // printf function calling for entering string
     fgets(str, sizeof(str), stdin);  //This will read the user input.           
     displayString(str);     // Passing string to a function.    
     return 0;
