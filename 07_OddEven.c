33b5b25cdb82212d63b480a510b44919e815dc73
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 17:29:46 +0530

    Update 07_OddEven.c

--- 07_OddEven.c
+++ 07_OddEven.c
@@ -1,10 +1,12 @@
-// A C Program to check whether the entered number is Odd or Even.
-#include <stdio.h>
-int main() {
-    int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num); //It will scan the numbers entered by user.
-    if(num % 2 == 0)
+//Author name : Aditya Bawnoo
+//Creation date : 22 March 2021
+// Purpose : Program to check whether the entered number is Odd or Even.
+#include <stdio.h> // preprocessive directive to include standard input and output
+int main() { // start of the main body
+    int num; // variable declaration
+    printf("Enter an number: "); // printf function calling for number
+    scanf("%d", &num); //scanf function calling for taking input of number entered by user.
+    if(num % 2 == 0) 
         printf("%d is even.", num);
     else
         printf("%d is odd.", num);
