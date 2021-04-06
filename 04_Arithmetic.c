8e536230d7bbbc37d5b64684abe738547611d749
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-06 16:46:18 +0530

    Update 04_Arithmetic.c

--- 04_Arithmetic.c
+++ 04_Arithmetic.c
@@ -1,29 +1,26 @@
-#include<stdio.h>
-int main(){
-    char operator;
-    double a, b;
-    printf("Enter an operator (+, -, *, /):"); //It will print the operater to be entered by user.
-    scanf("%c", &operator); //It will scan the operater entered by user.
-    printf("Enter two operands:");
-    scanf("%lf %lf",&a, &b); //It will scan the numbers entered by user.
-    switch(operator)
-    {
-        case '+':
-            printf("%.2lf + %.2lf = %.2lf",a, b, a+b); //Addition Function
-            break;
+//Author name : Aditya Bawnoo
+// Creation date : 18 March 2021
+//Purpose : Program for Arithmetic operators 
+#include<stdio.h> // Preprocessive directive  to include standard input and output.
+int main()
+{  //Start of main function body
+   
+   int num1, num2, add, subtract, multiply;  //variable declaration of integer data type
+   float divide;  //variable declaration of float data type
+ 
+   printf("Enter two numbers\n");   //printf function calling to print two numbers
+   scanf("%d%d", &num1, &num2);  //taking input from user
+ 
+   add = num1 + num2;   //formula for addition
+   subtract = num1 - num2;  //formula for subtraction
+   multiply = num1 * num2;  //formula for multiplication
+   divide = num1 / (float)num2;   //formula for division
 
-        case '-':
-            printf("%.2lf - %.2lf = %.2lf",a, b, a-b); //Subtraction Function
-            break;
-
-        case '*':
-            printf("%.2lf * %.2lf = %.2lf",a, b, a*b); //Multiplication Function
-            break;
-
-        case '/':
-            printf("%.2lf / %.2lf = %.2lf",a, b, a/b); //Division Function
-            break;
-    printf("Error encountered by the input values");
-    }
-    return 0;
+   printf("Sum = %d\n", add); //printf function calling to print sum
+   printf("Difference = %d\n", subtract); //printf function calling to print difference
+   printf("Multiplication = %d\n", multiply);   //printf function calling to print multiplication 
+   printf("Division = %.2f\n", divide);   //printf function calling to print division
+ 
+   return 0;   //return statement
 }
+//end of main function body
