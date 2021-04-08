846e64439a35a43e5b285ba3fff60e93589fb928
Adityabawnoo <76390953+Adityabawnoo@users.noreply.github.com>
2021-04-08 09:03:15 +0530

    Update 18_SearchArr.c

--- 18_SearchArr.c
+++ 18_SearchArr.c
@@ -1,39 +1,28 @@
 //Author name : Aditya Bawnoo 
 //Creation date : 20 March 2021
-// Purpose : A C Program representing Binary Search
+// Purpose : A C Program representing linear Search
 #include <stdio.h> // preprocessive directory to include standard input and output header file
 int main(){  // start of the main body
-  int c, first, last, middle, n, search, array[100]; // variable and array declaration
+  int a[50],size,i,val,flag=0;  // variable and array declaration
 
-  printf("Enter number of elements\n"); // printf function calling for entering elements
-  scanf("%d", &n);                      // scanf function calling for taking input of numbers entered by the user
-
-  printf("Enter %d integers\n", n);  // printf function calling for entering integers
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
-
-  printf("Enter value to find\n"); // printf function calling for enter the value to be find
-  scanf("%d", &search);  // scanf function calling for taking input of the search value
-
-  first = 0;
-  last = n - 1;
-  middle = (first+last)/2;
-
-  while (first <= last) {
-    if (array[middle] < search)
-      first = middle + 1;
-    else if (array[middle] == search) {
-      printf("%d found at location %d.\n", search, middle+1);
-      break;
+  printf("Enter Array elements\n"); // printf function calling for array elements
+  for (i=0;i<size;i++)
+    
+    scanf("%d",&a[i]);
+  printf("Enter value to search");
+  scanf("%d",&val);
+  
+  for (i=0;i<size; i++)
+  {
+    if (a[i] == val)
+    {
+      printf(" location = %d",i);
+      flag = 1;
     }
-    else
-      last = middle - 1;
-
-    middle = (first + last)/2;
   }
-  if (first > last)
-    printf("Not found! %d isn't present in the list.\n", search);
-
-  return 0;
-}  // end of the main body
+  if ( flag == 0)
+  {
+    printf("Not found");
+  }
+  getch ();
+}
